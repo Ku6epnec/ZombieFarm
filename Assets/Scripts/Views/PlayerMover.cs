e26d80ab06f2671ea23ba448cd2095aa1a800407
Alena Karpova <my.lovely.hulk@gmail.com>
2023-02-16 11:44:49 +0300

    add rotate in playerMover

--- Assets/Scripts/Views/PlayerMover.cs
+++ Assets/Scripts/Views/PlayerMover.cs
@@ -25,7 +25,8 @@ namespace ZombieFarm.Views
 
         private void FixedUpdate()
         {
-            StartMoving();
+            Move();
+            Rotate();
         }
 
         private void OnPlayerInputActionTriggered(InputAction.CallbackContext context)
@@ -42,13 +43,25 @@ namespace ZombieFarm.Views
             }
         }
 
-        private void StartMoving()
+        private void Move()
         {
-            Vector3 motion = new Vector3(moveCommand.x, 0, moveCommand.y);
-            motion *= movementSpeed * movementSpeed * Time.deltaTime;
+            Vector3 motion = new Vector3(-moveCommand.x, 0, -moveCommand.y);
+            motion *= movementSpeed * Time.deltaTime;
             motion.y -= gravity;
 
-            characterController.Move(transform.rotation * motion);
+            characterController.Move(motion);
+        }
+
+        private void Rotate()
+        {
+            if (moveCommand == Vector2.zero)
+            {
+                return;
+            }
+
+            Vector3 relativePos = new Vector3(-moveCommand.x, 0, -moveCommand.y);
+            Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+            transform.rotation = rotation;
         }
     }
 }
