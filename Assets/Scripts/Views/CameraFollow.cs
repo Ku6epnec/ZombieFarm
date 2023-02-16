9b058adc69e4a53ab65850e981f4480144caf658
Alena Karpova <my.lovely.hulk@gmail.com>
2023-02-16 14:58:50 +0300

    scene re-organization + camera follow

---
+++ Assets/Scripts/Views/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieFarm.Views
+{ 
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Vector3 cameraOffset;
+        [SerializeField] private Transform target;
+
+        private void LateUpdate()
+        { 
+            transform.position = target.position + cameraOffset;
+        }
+    }
+}
