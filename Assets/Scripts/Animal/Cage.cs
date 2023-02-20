6ed7d43955e165fe2bddffcea97a465f7da764e5
MrDoctor-666 <sheron.dragnil@mail.ru>
2023-02-20 15:34:52 +0300

    changes

--- Assets/Scripts/Animal/Cage.cs
+++ Assets/Scripts/Animal/Cage.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Release : MonoBehaviour
+public class Cage : MonoBehaviour
 {
     //count
     [SerializeField] float necessaryTime = 3f;
-    [SerializeField] Slider slider;
+    [SerializeField] Slider progressBar;
 
     private float elapsed = 0;
 
     private void Start()
     {
-        slider.maxValue = necessaryTime;
-        slider.gameObject.SetActive(false);
+        progressBar.maxValue = necessaryTime;
+        progressBar.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        slider.gameObject.SetActive(true);
-        slider.value = 0;
+        progressBar.gameObject.SetActive(true);
+        progressBar.value = 0;
     }
 
     private void OnTriggerStay(Collider other)
     {
         elapsed += Time.fixedDeltaTime;
-        slider.value = elapsed;
+        progressBar.value = elapsed;
         if (elapsed > necessaryTime) Free();
     }
 
     private void OnTriggerExit(Collider other)
     {
         elapsed = 0;
-        slider.gameObject.SetActive(false);
+        progressBar.gameObject.SetActive(false);
     }
 
     private void Free()
@@ -41,6 +41,6 @@ public class Release : MonoBehaviour
         //set animal to follow
 
         //delete the cage
-        gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 }
