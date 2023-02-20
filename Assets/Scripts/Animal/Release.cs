c13100e76c31bafa23a373095e3d40392ba2575d
MrDoctor-666 <sheron.dragnil@mail.ru>
2023-02-20 11:17:09 +0300

    release scripts

---
+++ Assets/Scripts/Animal/Release.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Release : MonoBehaviour
+{
+    //count
+    [SerializeField] float necessaryTime = 3f;
+    [SerializeField] float elapsed = 0;
+    [SerializeField] Slider slider;
+
+    private void Start()
+    {
+        slider.maxValue = necessaryTime;
+        slider.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        slider.gameObject.SetActive(true);
+        slider.value = 0;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        elapsed += Time.fixedDeltaTime;
+        slider.value = elapsed;
+        if (elapsed > necessaryTime) Free();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        elapsed = 0;
+        slider.gameObject.SetActive(false);
+    }
+
+    private void Free()
+    {
+        //set animal to follow
+
+        //delete the cage
+        gameObject.SetActive(false);
+    }
+}
