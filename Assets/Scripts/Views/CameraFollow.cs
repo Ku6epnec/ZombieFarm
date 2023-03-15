a0ac5669e35f4b6325dce3f2b368cb2f7da42683
Ku6epnec <ku6epnec@rambler.ru>
2023-03-15 04:31:45 +0300

    Add RayCast and TreeVariant

--- Assets/Scripts/Views/CameraFollow.cs
+++ Assets/Scripts/Views/CameraFollow.cs
@@ -11,14 +11,30 @@ namespace ZombieFarm.Views
         private void LateUpdate()
         { 
             transform.position = target.position + cameraOffset;
+            
+            RaycastHit[] allHits;
+            allHits = Physics.RaycastAll(transform.position, target.position, 100);
+            foreach (var hit in allHits)
+            {
+                if (!hit.transform.tag.Equals("Player"))
+                {
+                    Debug.Log("Мы пульнули лазером в объект: " + hit);
+                }
+            }
+            Debug.DrawRay(transform.position, -target.position, Color.black, 100);
         }
         private void OnTriggerEnter(Collider collider)
         {
             GameObject obstacle = collider.gameObject;
             MeshRenderer mesh = obstacle.GetComponent<MeshRenderer>();
-            Color _color = mesh.material.color;
-            _color.a = 0.1f;
-            mesh.material.color = _color;
+
+            foreach (Material material in mesh.materials)
+            {
+                Color _color = material.color;
+                _color.a = 0.2f;
+                material.color = _color;
+            }
+
             Debug.Log("Обзор загораживает: " + obstacle);
         }
 
