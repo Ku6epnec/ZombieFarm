f338e643620c3b020fb982205eb6fcff807e0980
Alena Karpova <95620549+AlenaKarp@users.noreply.github.com>
2023-03-26 17:17:20 +0300

    update vs package && add axe

--- Assets/Scripts/Views/Player/MainCharAnimator.cs
+++ Assets/Scripts/Views/Player/MainCharAnimator.cs
@@ -6,6 +6,8 @@ namespace ZombieFarm.Views.Player
     [RequireComponent(typeof(PlayerView), typeof(Animator))]
     public class MainCharAnimator : MonoBehaviour
     {
+        [SerializeField] Transform axe;
+
         private PlayerView playerView;
         private Animator animator;
         private float motionSpeed = 0;
@@ -48,6 +50,7 @@ namespace ZombieFarm.Views.Player
 
         private void OnChangeState(PlayerState newState)
         {
+            axe.gameObject.SetActive(newState == PlayerState.Attack);
             animator.SetInteger(animatorParameter_StateIndex_Int_Id, (int)newState);
         }
     }
