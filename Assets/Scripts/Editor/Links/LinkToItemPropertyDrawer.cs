75116b22c5b345d69567c2e0515de2a5a1563f5f
Alena Karpova <a_karpova2@lesta.group>
2023-04-18 08:59:31 +0300

    links && package

---
+++ Assets/Scripts/Editor/Links/LinkToItemPropertyDrawer.cs
@@ -0,0 +1,17 @@
+﻿
+using ZombieFarm.Config.LinkTargets;
+using UnityEditor;
+using UnityEngine;
+using UnityTools.Editor.Links;
+
+namespace CozyIslands.Editor.Links
+{
+    [CustomPropertyDrawer(typeof(LinkToItem), true)]
+    public class LinkToItemPropertyDrawer : LinkToConfigPropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            DrawLink<Item>(position, property, label, nameof(LinkToItem.itemId));
+        }
+    }
+}
