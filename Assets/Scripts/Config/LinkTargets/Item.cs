441a3d63d92f7893d3cdf6449dbaaf1072de23e2
Alena Karpova <a_karpova2@lesta.group>
2023-04-18 08:59:31 +0300

    links && package

---
+++ Assets/Scripts/Config/LinkTargets/Item.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZombieFarm.Config.LinkTargets
+{
+    [CreateAssetMenu(fileName = nameof(Item), menuName = "Configs" + "/" + nameof(Item))]
+    public class Item : ScriptableObject
+    {
+        [Serializable]
+        public struct Data
+        {
+            [Serializable]
+            public struct Currency
+            {
+            }
+
+            [Serializable]
+            public struct Score
+            {
+            }
+
+            [Serializable]
+            public struct Tool
+            {
+                public Operation[] operations;
+                public long maxDurability;
+            }
+
+            [Serializable]
+            public struct Material
+            {
+            }
+
+            [Serializable]
+            public struct CraftRecipe
+            {
+                public int playerLevelToUnlock;
+                public int input;
+                public int output;
+                public long duration;
+            }
+
+            [Serializable]
+            public struct Operation
+            {
+                public int type;
+            }
+
+            public int type;
+            public float worth;
+            public Currency currency;
+            public Score score;
+            public Tool tool;
+            public Material material;
+            public CraftRecipe craftRecipe;
+        }
+
+        public Sprite sprite;
+        public bool isSearchable;
+        public GameObject viewPrefab;
+        public GameObject inHandPrefab;
+        public Data data;
+    }
+}
