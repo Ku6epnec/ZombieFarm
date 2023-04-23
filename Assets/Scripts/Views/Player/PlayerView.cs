70d2ab733ffe59390c14ad03579c71b8cb7bfbfa
Ku6epnec <ku6epnec@rambler.ru>
2023-04-23 21:55:07 +0300

    Fixes v0.03

--- Assets/Scripts/Views/Player/PlayerView.cs
+++ Assets/Scripts/Views/Player/PlayerView.cs
@@ -47,8 +47,6 @@ namespace ZombieFarm.Views.Player
 
         private void Update()
         {
-            Debug.Log("Root.Player.CurrentMotionSpeed = " + Root.Player.CurrentMotionSpeed);
-            Debug.Log("destroyObjectState = " + destroyObjectState);
             if (Root.Player.CurrentMotionSpeed > deltaSpeed && destroyObjectState == false)
             {
                 RefreshCurrentState(PlayerState.Idle);
