c9c6943aca51b6889f7d5d85503ee64d2cd8d63a
Ku6epnec <ku6epnec@rambler.ru>
2023-04-23 16:10:50 +0300

    Add Camera & Joystick

--- Assets/Scripts/Views/CameraFollow.cs
+++ Assets/Scripts/Views/CameraFollow.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 namespace ZombieFarm.Views
@@ -7,10 +6,51 @@ namespace ZombieFarm.Views
     {
         [SerializeField] private Vector3 cameraOffset;
         [SerializeField] private Transform target;
+        [SerializeField][Range(0, 1)] private float ivisibleForObstacles;
+
+        private float colorMaxValue = 1.0f;
 
         private void LateUpdate()
         { 
             transform.position = target.position + cameraOffset;
+        }
+        private void OnTriggerEnter(Collider collider)
+        {
+            try
+            {
+                GameObject obstacle = collider.gameObject;
+                MeshRenderer mesh = obstacle.GetComponent<MeshRenderer>();
+
+                foreach (Material material in mesh.materials)
+                {
+                    Color _color = material.color;
+                    _color.a = ivisibleForObstacles;
+                    material.color = _color;
+                }
+            }
+            catch 
+            {
+
+            }
+        }
+
+        private void OnTriggerExit(Collider collider)
+        {
+            try
+            {
+                GameObject obstacle = collider.gameObject;
+                MeshRenderer mesh = obstacle.GetComponent<MeshRenderer>();
+                foreach (Material material in mesh.materials)
+                {
+                    Color _color = material.color;
+                    _color.a = colorMaxValue;
+                    material.color = _color;
+                };
+            }
+            catch
+            {
+
+            }
         }
     }
 }
