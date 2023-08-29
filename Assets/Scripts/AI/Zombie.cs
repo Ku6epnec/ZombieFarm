1172b14f72d4cdafc888b0523787c01e9e9f17f2
Alena Karpova <95620549+AlenaKarp@users.noreply.github.com>
2023-08-30 00:07:11 +0300

    fix zombie

--- Assets/Scripts/AI/Zombie.cs
+++ Assets/Scripts/AI/Zombie.cs
@@ -48,7 +48,7 @@ namespace ZombieFarm.AI
         [SerializeField][Range(0, 1000)] private int walkingProbability;
 
         private float timer;
-        private float maxTimer = 2.0f;
+        private float maxTimer = 1.0f;
         private float recievedDamageTimer;
 
         private bool IsCurrentStateUpdatableInEveryFrame => currentState == ZombieState.Chase;
@@ -72,6 +72,7 @@ namespace ZombieFarm.AI
         private void Start()
         {
             currentState = ZombieState.Idle;
+
             playerView = Root.ViewManager.GetPlayerView();
         }
 
@@ -85,13 +86,19 @@ namespace ZombieFarm.AI
         private void FixedUpdate()
         {
             RefreshCurrentState();
-            timer -= Time.deltaTime;
+
             recievedDamageTimer -= Time.deltaTime;
-            if (currentState == ZombieState.Attack && timer <= 0)
+
+            if (currentState == ZombieState.Attack)
             {
-                Attack();
-                timer = maxTimer;
+                timer -= Time.deltaTime;
+
+                if (timer <= 0)
+                { 
+                    Attack();
+                }
             }
+
             if (IsCurrentStateUpdatableInEveryFrame == true)
             {
                 UpdateAction(currentState);
@@ -120,6 +127,8 @@ namespace ZombieFarm.AI
             agent.isStopped = true;
 
             playerView.ReceivedDamage(Damage);
+
+            timer = maxTimer;
         }
 
         public void ReceivedDamage(float damage)
@@ -159,8 +168,7 @@ namespace ZombieFarm.AI
             switch (newState)
             {
                 case ZombieState.Attack:
-                    if (timer <= 0)
-                    Attack();
+                    timer = maxTimer;
                     break;
 
                 case ZombieState.Chase:
@@ -188,7 +196,7 @@ namespace ZombieFarm.AI
             float distanceToPlayer = Vector3.Distance(this.transform.position, Root.Player.transform.position);
 
             if (distanceToPlayer < attackDistance)
-            { 
+            {
                 return ZombieState.Attack;
             }
 
