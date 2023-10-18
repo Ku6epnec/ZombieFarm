5791e20bd6e0e6bea8e9e24d94d19fff7eaf402f
MrDoctor-666 <sheron.dragnil@mail.ru>
2023-10-18 20:36:47 +0300

    disable zombie on death

--- Assets/Scripts/Loot/OnDisableLoot.cs
+++ Assets/Scripts/Loot/OnDisableLoot.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace ZombieFarm.Loot
 {
-    public class OnDisableObject : Loot
+    public class OnDisableLoot : Loot
     {
         private void OnDisable()
         {
