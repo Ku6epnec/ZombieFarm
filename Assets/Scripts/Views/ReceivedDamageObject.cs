b9e75d5e097ccd03bf45aaa6fa93037fde7a1cf8
Ku6epnec <ku6epnec@rambler.ru>
2023-12-06 18:52:14 +0300

    Fixed Targetting Player 2.0

--- Assets/Scripts/Views/ReceivedDamageObject.cs
+++ Assets/Scripts/Views/ReceivedDamageObject.cs
@@ -4,7 +4,7 @@ using System;
 public class ReceivedDamageObject: MonoBehaviour
 {
     internal virtual event Action CleanInteractiveObject = () => {};
-
+    public bool Active = true;
     public virtual void Interaction(float interactiveValue)
     {
 
