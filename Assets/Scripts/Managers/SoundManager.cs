d9c31a4f7b7b8443fa5c27802572eda1cd98f3a5
Ku6epnec <ku6epnec@rambler.ru>
2024-08-14 17:17:10 +0300

    Add sound on Farm

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,25 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
     public AudioClip[] Music;
-    public AudioClip[] Sounds;
 
     [SerializeField] private AudioSource musicSource;
-    [SerializeField] private AudioSource soundSource;
+
 
     void Awake()
     {
         musicSource.clip = Music[0];
-        musicSource.Play(); 
-        
-    }
+        musicSource.Play();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
